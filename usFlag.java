b0b12870f509d20c1ff9787f4458aaf4e038a3fd
Samuel <Samueladeyemi2006@gmail.com>
2025-01-19 07:52:36 +0100

    usa flag

---
+++ usFlag.java
@@ -0,0 +1,13 @@
+public class Flag2 {
+    public class void main(String[] args){
+        int flagHeight = 6;
+        for(int i = 0; i < flagHeight; i++){
+            String row = " ";
+            if (i < 3){
+                row = "****=======****";
+            }else{
+                row = "=====  =====";
+            }
+        }
+    }
+}
